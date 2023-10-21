0739e3a2b069248747bd2163c10d4d0cf385b5c1
olexanderf <alex1990sfreem@gmail.com>
2023-10-21 22:38:30 +0300

    end 95 spl filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,9 +6,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedPage = ['page', 'sort', 'limit', 'fields'];
+
+    excludedPage.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // Execute query
+    const tours = await query;
+
+    // Send response
     res.status(200).json({
-      status: 'succes',
+      status: 'success',
       results: tours.length,
       data: { tours }
     });
@@ -22,7 +33,7 @@ exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
     res.status(200).json({
-      status: 'succes',
+      status: 'success',
       data: { tour }
     });
   } catch (error) {
@@ -36,7 +47,7 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
-      message: 'succes',
+      message: 'success',
       data: {
         tour: newTour
       }
@@ -56,7 +67,7 @@ exports.updateTour = async (req, res) => {
       runValidators: true
     });
     res.status(200).json({
-      message: 'succes',
+      message: 'success',
       data: {
         tour
       }
@@ -73,7 +84,7 @@ exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
-      message: 'succes',
+      message: 'success',
       data: null
     });
   } catch (error) {
