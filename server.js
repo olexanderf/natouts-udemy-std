f4ebe8a6dc2c28109141ea1ed7e7d576f2145203
olexanderf <alex1990sfreem@gmail.com>
2023-11-15 21:22:46 +0200

    l123 end of error handling

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ mongoose
   .then(() => console.log('DB Connection successful'));
 // console.log(DB);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting down...');
+  console.log(err);
+  server.close(() => process.exit(1));
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Shutting down...');
+  console.log(err);
+  process.exit(1);
 });
