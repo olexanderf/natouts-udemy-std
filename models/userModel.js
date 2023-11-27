94699c12515af9bc0b0813915234fe2595157c14
olexanderf <alex1990sfreem@gmail.com>
2023-11-27 20:39:26 +0200

    l129 jwt token

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,6 +44,15 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords don`t match'
     }
   }
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
