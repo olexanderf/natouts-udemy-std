e8b8ff07842b1d03e5c23c32e8e723079897645c
olexanderf <alex1990sfreem@gmail.com>
2023-12-27 14:35:36 +0200

    l142 add token to cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,18 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOption = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+
+  res.cookie('jwt', token, cookieOption);
+
+  // Remove password from the output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
