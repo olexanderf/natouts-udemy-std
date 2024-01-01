4eda27984ac957438a30e49d4434bec850a5eafa
olexanderf <alex1990sfreem@gmail.com>
2024-01-01 17:41:34 +0200

    l150 modeling geospatial data

--- models/tourModel.js
+++ models/tourModel.js
@@ -75,7 +75,32 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    starLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: 'String',
+      description: 'String'
+    },
+    // Embedded relations in document
+    locations: [
+      {
+        type: {
+          type: 'String',
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: 'String',
+        description: 'String',
+        day: Number
+      }
+    ]
   },
   { toJSON: { virtuals: true } },
   { toObject: { virtuals: true } }
