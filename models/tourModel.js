701af9a0978cb6772dce9ea6cfd937be7cd88c7f
olexanderf <alex1990sfreem@gmail.com>
2024-01-03 20:24:10 +0200

    add review model

--- models/tourModel.js
+++ models/tourModel.js
@@ -143,16 +143,16 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  next();
-});
-
 tourSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'guides',
     select: '-__v -passwordChangedAt'
   });
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
 });
 
