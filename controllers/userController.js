1f7003db4e00e82f4eb287cc99f9fc49eccc382c
olexanderf <alex1990sfreem@gmail.com>
2024-01-07 18:37:11 +0200

    l164 get me rout

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,6 +35,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
