27ebacd36a9f73f64eeaf1b30dc7c827bfc64457
olexanderf <alex1990sfreem@gmail.com>
2024-01-15 23:21:25 +0200

    l171 start with pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,11 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //1) GLOBAL MIDDLEWARES
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
@@ -40,8 +45,6 @@ app.use(
   })
 );
 
-app.use(express.static(`${__dirname}/public`));
-
 const limiter = rateLimit({
   limit: 100,
   windowMS: 60 * 60 * 1000,
@@ -56,6 +59,12 @@ app.use((req, res, next) => {
 });
 
 //3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas'
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
